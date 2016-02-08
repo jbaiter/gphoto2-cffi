c0ad75f82264d36c9fcc141aa2327885a2cd6c55
Johannes Baiter <johannes.baiter@gmail.com>
2016-02-08 12:07:02 +0100

    Add accidentally ommited backend_build module

---
+++ gphoto2cffi/backend_build.py
@@ -0,0 +1,20 @@
+import os
+
+from cffi import FFI
+
+with open(os.path.join(os.path.dirname(__file__), 'gphoto2.cdef')) as fp:
+    CDEF = fp.read()
+
+SOURCE = """
+#include "gphoto2/gphoto2-version.h"
+#include "gphoto2/gphoto2-context.h"
+#include "gphoto2/gphoto2-camera.h"
+#include <time.h>
+"""
+
+ffi = FFI()
+ffi.set_source("_backend", SOURCE, libraries=['gphoto2'])
+ffi.cdef(CDEF)
+
+if __name__ == "__main__":
+    ffi.compile()
